abbe14e65b3b5d0dec475799dfd24c40934e968c
ZITANIMohamed98 <zitaniceo@gmail.com>
2023-08-03 20:22:08 +0200

    finalized the gallery

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { SubHeading} from '../../components';
+import { images } from '../../constants';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app_bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+
+    </div>
+    <div className='app__wrapper_img'>
+        <img src={images.findus} alt='findus'/>
+    </div>
   </div>
 );
 
